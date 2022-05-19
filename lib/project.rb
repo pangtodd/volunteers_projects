a8adbdad84d6e08d175a816793b23fa092bd3bea
Todd Pangilinan <pang.todd@gmail.com>
2022-05-19 14:46:06 -0700

    all method partially passes. Need to develeop save method to pass other half.

--- lib/project.rb
+++ lib/project.rb
@@ -10,4 +10,21 @@ class Project
   def ==(project_to_compare)
     self.title() == project_to_compare.title()
   end
+
+  def self.all
+    returned_projects= DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id=>id}))
+    end
+    projects
+  end
+
+  # def save
+  #   result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  #   @id = result.first().fetch("id").to_i
+  # end
+
 end

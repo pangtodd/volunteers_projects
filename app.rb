38e0782efa1072a4e66d34ca1af726c334c4bcaf
Todd Pangilinan <pang.todd@gmail.com>
2022-05-20 10:44:00 -0700

    establish new_project.erb and supporting routes in app.rb.

--- app.rb
+++ app.rb
@@ -8,4 +8,22 @@ also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
+get('/') do
+  @projects = Project.all
+  erb(:projects)
+end
 
+get('/projects')
+  @projects = Project.all
+  erb(:projects)
+
+  post('/projects') do
+    name = params[:project_title]
+    project = Project.new(name, nil)
+    project.save()
+    erb(:projects)
+  end
+
+get('/projects/new') do
+  erb(:new_project)
+end

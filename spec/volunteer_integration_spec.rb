fe8f19fa14f7f0b815dea12a03e1a2ea6b29c4dd
Todd Pangilinan <pang.todd@gmail.com>
2022-05-20 23:07:01 -0700

    establish intergration test for Volunteer creation path.

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -5,10 +5,6 @@ require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# # Your project should be set up so that a volunteer can only be created if a project already exists. (This makes it easier to assign the one to many relationship in Sinatra.) Focus on getting one integration spec passing at a time.
-
-# # The user should be able to visit the home page and fill out a form to add a new project. When that project is created, the application should direct them back to the homepage.
-
 describe 'the project creation path', {:type => :feature} do
   it 'takes the user to the homepage where they can create a project' do
     visit '/'
@@ -19,7 +15,6 @@ describe 'the project creation path', {:type => :feature} do
   end
 end
 
-# # A user should be able to click on a project to see its detail. The detail page includes a form where the project can be updated. When the form is submitted, the user can be directed to either the home page or that project's detail page. (The test will work for either.)
 
 describe 'the project update path', {:type => :feature} do
   it 'allows a user to change the name of the project' do
@@ -34,7 +29,6 @@ describe 'the project update path', {:type => :feature} do
   end
 end
 
-# # A user should be able to nagivate to a project's detail page and delete the project. The user will then be directed to the index page. The project should no longer be on the list of projects.
 
 describe 'the project delete path', {:type => :feature} do
   it 'allows a user to delete a project' do
@@ -48,8 +42,6 @@ describe 'the project delete path', {:type => :feature} do
   end
 end
 
-# # The user should be able to click on a project detail page and see a list of all volunteers working on that project. The user should be able to click on a volunteer to see the volunteer's detail page.
-
 describe 'the volunteer detail page path', {:type => :feature} do
   it 'shows a volunteer detail page' do
     test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
@@ -59,5 +51,16 @@ describe 'the volunteer detail page path', {:type => :feature} do
     test_volunteer.save
     visit "/projects/#{project_id}"
     expect(page).to have_content('Jasmine')
+  end
+end
+
+describe 'the volunteer creation path', {:type => :feature} do
+  it 'creates a volunteer' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    project_id = test_project.id.to_i
+    visit "/projects/#{project_id}"
+    fill_in('volunteer_name', :with => 'Skinny Pete')
+    expect(page).to have_content('Skinny Pete')
   end
 end
